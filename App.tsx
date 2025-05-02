463bd1325558fcf1061b6f58cb6d30c8942d624b
JoaoPedro <cpsenha@gmail.com>
2025-05-02 15:49:06 -0300

    Removed unnecessary imports from the main App file

--- App.tsx
+++ App.tsx
@@ -1,14 +1,13 @@
 import { SafeAreaView } from 'react-native-safe-area-context';
 
 import { StatusBar } from 'expo-status-bar';
-import { useFonts, Karla_300Light, Karla_700Bold, Karla_400Regular } from '@expo-google-fonts/karla';
+import { useFonts, Karla_700Bold, Karla_400Regular } from '@expo-google-fonts/karla';
 
 import { GluestackUIProvider } from '@gluestack-ui/themed';
 import { View } from '@gluestack-ui/themed';
 import { config } from './config/gluestack-ui.config';
 
 import { Loading } from '@components/Loading';
-import { Login } from '@screens/Auth/Login';
 
 import { AuthProvider } from '@contexts/manager.routes';
 import { Routes } from '@routes/index';
